a1999362b2d982ec4dc377f5b331211febf9b0de
Amartya Ghosh <amartyaghosh48@gmail.com>
2025-09-04 17:15:46 +0530

    add context api

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import bg from "../assets/authBg.png";
 import { IoEye } from "react-icons/io5";
 import { IoEyeOff } from "react-icons/io5";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+import { userDataContext } from "../context/userContext";
 
 const SignUp = () => {
-
   const [showPassword, setShowPassword] = useState(false);
-  const navigate = useNavigate()
+  const { serverUrl } = useContext(userDataContext);
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = async () => {};
 
   return (
     <div
@@ -18,27 +24,63 @@ const SignUp = () => {
         <h1 className="text-white text-[30px] font-semibold mb-[30px]">
           Register to <span className="text-blue-400">Virtual Assistant</span>
         </h1>
+        {/* Name Input */}
         <input
           type="text"
           placeholder="Enter Your Name"
           className="w-full h-[60px] outline-none border-2 border-white bg-transparent text-white placeholder-gray-300 px-[20px] py-[10px] rounded-full text-[18px]"
+          required
+          onChange={(e) => setName(e.target.value)}
+          value={name}
         />
+        {/*  Email input */}
         <input
           type="email"
           placeholder="Enter Your Email"
           className="w-full h-[60px] outline-none border-2 border-white bg-transparent text-white placeholder-gray-300 px-[20px] py-[10px] rounded-full text-[18px]"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
         />
+        {/* Password */}
         <div className="w-full h-[60px] border-2 border-white bg-transparent text-white rounded-full text-[18px] relative">
-          <input type={showPassword?"text":"password"} placeholder="Password" className="w-full h-full rounded-full outline-none bg-transparent placeholder-gray-300 px-[20px] py-[10px]" />
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            className="w-full h-full rounded-full outline-none bg-transparent placeholder-gray-300 px-[20px] py-[10px]"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
 
           {/* function of hidden password */}
-          {!showPassword && <IoEye className="absolute top-[18px] right-[20px] w-[25px] h-[25px] text-white cursor-pointer" onClick={()=>setShowPassword(true)}/>}
-          {showPassword && <IoEyeOff className="absolute top-[18px] right-[20px] w-[25px] h-[25px] text-white cursor-pointer" onClick={()=>setShowPassword(false)}/>}
+          {!showPassword && (
+            <IoEye
+              className="absolute top-[18px] right-[20px] w-[25px] h-[25px] text-white cursor-pointer"
+              onClick={() => setShowPassword(true)}
+            />
+          )}
+          {showPassword && (
+            <IoEyeOff
+              className="absolute top-[18px] right-[20px] w-[25px] h-[25px] text-white cursor-pointer"
+              onClick={() => setShowPassword(false)}
+            />
+          )}
         </div>
 
         {/* Sign Up button */}
-        <button className="min-w-[150px] mt-[30px] h-[60px] text-black font-semibold bg-white rounded-full text-[19px]">Sign Up</button>
-         <p className="text-white text-[18px]">Already have an account ? <span className="text-blue-500 cursor-pointer" onClick={()=>navigate("/signin")}>Sign In</span></p>
+        <button className="min-w-[150px] mt-[30px] h-[60px] text-black font-semibold bg-white rounded-full text-[19px]">
+          Sign Up
+        </button>
+        <p className="text-white text-[18px]">
+          Already have an account ?{" "}
+          <span
+            className="text-blue-500 cursor-pointer"
+            onClick={() => navigate("/signin")}
+          >
+            Sign In
+          </span>
+        </p>
       </form>
     </div>
   );
