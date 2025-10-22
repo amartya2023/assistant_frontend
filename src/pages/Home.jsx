557306af0eab810c051ad8c08d3c9505d90fd679
Amartya Ghosh <amartyaghosh48@gmail.com>
2025-10-23 03:52:23 +0530

    Setting Up Speech Recognition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,60 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { userDataContext } from "../context/userContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Home = () => {
-  const { userData, serverUrl, setUserData } = useContext(userDataContext);
+  const { userData, serverUrl, setUserData, getGeminiResponse } = useContext(userDataContext);
   const navigate = useNavigate();
 
-  const handleLogout = async ()=> {
+  const handleLogout = async () => {
     try {
-      const result = await axios.get(`${serverUrl}/api/auth/logout`, {withCredentials:true});
+      const result = await axios.get(`${serverUrl}/api/auth/logout`, {
+        withCredentials: true,
+      });
       setUserData(null);
       navigate("/signin");
     } catch (error) {
       setUserData(null);
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
+
+  useEffect(() => {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    recognition.continuous = true;
+    recognition.lang = "en-US";
+
+    recognition.onresult = async(e) => {
+      const transcript = e.results[e.results.length-1][0].transcript.trim();
+      console.log(transcript)
+
+      if(transcript.toLowerCase().includes(userData.assistantName.toLowerCase())){
+        const data = await getGeminiResponse(transcript);
+        console.log(data)
+      }
+    }
+
+    recognition.start();
+  }, []);
 
   return (
     <div className="w-full h-[100vh] bg-gradient-to-t from-[black] to-[#030353] flex justify-center items-center flex-col gap-[15px]">
       <button
-          className="min-w-[150px] mt-[30px] h-[60px] text-black font-semibold bg-white absolute top-[20px] right-[20px] rounded-full text-[19px] cursor-pointer hover:bg-blue-600"
-          onClick={handleLogout}
-        >
-          Logout
-        </button>
-        <button
-          className="min-w-[150px] mt-[30px] h-[60px] text-black font-semibold absolute top-[100px] right-[20px] bg-white rounded-full text-[19px] cursor-pointer px-[20px] py-[10px] hover:bg-blue-600"
-          onClick={()=>navigate("/customize")}
-        >
-          Customize Your Assistant
-        </button>
+        className="min-w-[150px] mt-[30px] h-[60px] text-black font-semibold bg-white absolute top-[20px] right-[20px] rounded-full text-[19px] cursor-pointer hover:bg-blue-600"
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
+      <button
+        className="min-w-[150px] mt-[30px] h-[60px] text-black font-semibold absolute top-[100px] right-[20px] bg-white rounded-full text-[19px] cursor-pointer px-[20px] py-[10px] hover:bg-blue-600"
+        onClick={() => navigate("/customize")}
+      >
+        Customize Your Assistant
+      </button>
       <div className="w-[300px] h-[400px] flex justify-center items-center overflow-hidden rounded-4xl shadow-lg">
         <img
           src={userData?.assistantImage}
